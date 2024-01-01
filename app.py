c86718ef9f6dab0f28d942e019c54a671134661c
oceandi <126319987+oceandi@users.noreply.github.com>
2024-01-01 23:34:08 +0300

    fuck me

--- app.py
+++ app.py
@@ -3,14 +3,31 @@ import requests
 import os
 import openai
 from dotenv import load_dotenv
+import json
+from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
+app.secret_key = os.getenv("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+oauth.register(
+    "auth0",
+    client_id=os.getenv("AUTH0_CLIENT_ID"),
+    client_secret=os.getenv("AUTH0_CLIENT_SECRET"),
+    client_kwargs={"scope": "openid profile email"},
+    server_metadata_url=f'https://dev-r7aunaebn4u68kqq.us.auth0.com/.well-known/openid-configuration',
+    api_base_url=f'https://dev-r7aunaebn4u68kqq.us.auth0.com',
+    access_token_url=f'https://dev-r7aunaebn4u68kqq.us.auth0.com/oauth/token',
+    authorize_url=f'https://dev-r7aunaebn4u68kqq.us.auth0.com/authorize',
+)
 
 # OpenAI API'yi kullanarak sohbet tamamlama işlevi
 def get_chat_response(message):
+    api_key = os.getenv("OPENAI_API_KEY")
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {api_key}"
@@ -25,9 +42,45 @@ def get_chat_response(message):
     else:
         return "Üzgünüm, bir yanıt alamadım."
 
+@app.route("/login")
+def login_page():
+    return render_template('login.html')
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://"
+        + os.getenv("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("index", _external=True),
+                "client_id": os.getenv("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # Kullanıcı giriş yapmışsa, onları anasayfaya yönlendir
+    if 'user' in session:
+        return render_template('index.html', session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    else:
+        return redirect(url_for('login_page'))
+
+@app.route("/auth0-login")
+def auth0_login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
 
 @app.route("/api", methods=["POST"])
 def api():
@@ -48,5 +101,5 @@ def auth_response():
     # Örneğin kullanıcının profil sayfasına veya ana sayfaya
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=os.getenv("PORT", 3000))
